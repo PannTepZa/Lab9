e47fd8f778079120db1d0d0b665f008eb4566a7c
PannTepZa <pannkitina@gmail.com>
2025-12-19 14:48:52 +0700

    lab9_2cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -19,20 +19,5 @@ char printO(int rows, int cols){
 
 int main(){
 	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
 	return 0;
 }
